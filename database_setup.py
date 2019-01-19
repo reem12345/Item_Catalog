ac76f8d8a4cdcfecc4246d89210c4ae20f6dadf6
Reem Alsulami <44661856+reem12345@users.noreply.github.com>
2019-01-19 09:46:05 +0300

    Add files via upload

---
+++ database_setup.py
@@ -0,0 +1,66 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class Book(Base):
+    __tablename__ = 'book'
+
+    name = Column(String(100), nullable=False)
+    id = Column(Integer, primary_key=True)
+    file_link = Column(String(250))
+    picture = Column(String(250))
+    author = Column(String(80), nullable=False)
+    yearOfEmission = Column(String(10))
+    numOfPage = Column(String(50))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+            'file_link': self.file_link,
+            'picture': self.picture,
+            'author': self.author,
+            'yearOfEmission': self.yearOfEmission,
+            'numOfPage': self.numOfPage
+        }
+
+
+engine = create_engine('sqlite:///catalog.db')
+
+
+Base.metadata.create_all(engine)
